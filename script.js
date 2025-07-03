d7e3a8fe23e3256b44611f7e4b81d717da783909
Sajad <sajadsoorni@gmail.com>
2025-07-03 11:48:50 +0100

    feat: implement sequential AJAX calls with nested callbacks
    - Created getCountryAndNeighbour() function
    - Made second AJAX call only after first completes (based on border code)
    - Used /v2/alpha endpoint to fetch data for neighboring country
    - Added renderCountry() helper with optional class for neighbor styling
    - Demonstrated callback hell with nested setTimeout examples

--- script.js
+++ script.js
@@ -11,17 +11,9 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `
-    <article class="country">
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
     <img class="country__img" src="${data.flag}" />
     <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
@@ -34,11 +26,54 @@ const getCountryData = function (country) {
       </div>
       </article>`;
 
-    countriesContainer.insertAdjacentHTML(`beforeend`, html);
-    countriesContainer.style.opacity = 1;
+  countriesContainer.insertAdjacentHTML(`beforeend`, html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // Ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    // Render country 1
+    renderCountry(data);
+
+    // Get neighbour country
+    // const [neighbour] = data.borders;
+    const neighbour = data.borders?.[0];
+    if (!neighbour) return;
+
+    // Ajax call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('portugal');
-getCountryData('usa');
-getCountryData('germany');
+// getCountryAndNeighbour('portugal');
+getCountryAndNeighbour('usa');
+
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
